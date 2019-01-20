094f23dbe6e42af176a41d918c6e99253adac07f
DobienC <dobien_c@yahoo.com>
2019-01-20 14:05:32 -0800

    new functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,55 @@
 var intervalID;
 var resultIntervalID;
-var time = 5;
+var time = 2;
 var delayTime = 0;
+var ansSelected = false;
+var ans = -1;
+var curQ = -1;
 
 var questions = [
-    {question: "Q", answer: "Y", options: ["1","2","3"]},
-    {question: "Q", answer: "Y", options: ["1","2","3"]},
-    {question: "Q", answer: "Y", options: ["1","2","3"]},
-    {question: "Q", answer: "Y", options: ["1","2","3"]},
+    {question: "What isn't a color in MTG?", answer: "2", options: ["black","yellow","green"]},
+    {question: "What isn't a Ravnica Guild?", answer: "1", options: ["Algor","Simic","Dimir"]},
+    {question: "What isn't a MTG spell?", answer: "2", options: ["Enchantment","Illusion","Creature"]},
+    {question: "What isn't a MTG plane", answer: "2", options: ["Ravnica","Kaer","Tarkir"]},
 ];
 
 intervalID = setInterval(countDown, 1000);
 
+
+function checkAnswer(){
+    if(ansSelected === true){
+        ansSelected = false;
+        if(ans === questions[curQ].answer){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
 function countDown(){
     if(time >= 0){
+        checkAnswer();
         $("#tester").text(time);    
         console.log(time);
         time--;
     } else {
-        clearInterval(intervalID);
-        time = 5
-        resultIntervalID = setInterval(delayResult,1000);
-
+        curQ++;
+        if(curQ > questions.length){
+            console.log(curQ);
+            alert("Done");
+            clearInterval(intervalID);
+        } else {
+            console.log(curQ); 
+            clearInterval(intervalID);
+            time = 2
+            resultIntervalID = setInterval(delayResult,1000);   
+        }
     }
 }
     
 function delayResult(){
-    if(delayTime <= 5){
+    if(delayTime <= 1){
         delayTime++;
         console.log("Delay: " + delayTime);
     } else {
@@ -36,8 +59,8 @@ function delayResult(){
     }
 }
 
-for(var i = 0; i < questions.length; i++){
-    console.log(questions[i].question);
-    console.log(questions[i].answer);
-    console.log(questions[i].options);
-}
+// for(var i = 0; i < questions.length; i++){
+//     console.log(questions[i].question);
+//     console.log(questions[i].answer);
+//     console.log(questions[i].options);
+// }
