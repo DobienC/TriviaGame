1dfb5ff6dd1b0ecb2b295a73f37e2af2b65dbe76
DobienC <dobien_c@yahoo.com>
2019-01-20 21:04:47 -0800

    Complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TriviaGame
+# TriviaGame: Sacramento Kings Edition
+
+This game is a basic trivia quiz based on the Sacramento Kings Basketball Team. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var intervalID;
 var resultIntervalID;
-var timeLimit = 3;
+var timeLimit = 30;
 var time = timeLimit;
 var delayTime = 0;
 var ansSelected = false;
@@ -12,13 +12,19 @@ var wrong = 0;
 var timeOut = 0;
 
 var questions = [
-    {question: "What isn't a color in MTG?", answer: "1", options: ["black","yellow","green"]},
-    {question: "What isn't a Ravnica Guild?", answer: "0", options: ["Algor","Simic","Dimir"]},
-    {question: "What isn't a MTG spell?", answer: "1", options: ["Enchantment","Illusion","Creature"]},
-    {question: "What isn't a MTG plane", answer: "1", options: ["Ravnica","Kaer","Tarkir"]},
+    {gif: "assets/images/KC.gif",question: "What city did the Kings reside in before Sacramento?", answer: "1", options: ["Anaheim","Kansas City","Seattle","Montreal"]},
+    {gif: "assets/images/RayAllen.gif",question: "Which one of these players were not part of the Sacramento Kings?", answer: "0", options: ["Ray Allen","Tyreke Evans","Jason Williams","Brad Miller"]},
+    {gif: "assets/images/Chris.gif",question: "Which past Kings player never won a ring?", answer: "0", options: ["Chris Webber","Peja Stojakovic","Jason Williams", "Scot Pollard"]},
+    {gif: "assets/images/Punch.gif",question: "When was the last time the Kings made it to the playoffs?", answer: "1", options: ["2009","2006","2002","2015"]},
+    {gif: "assets/images/Peja.gif",question: "Who wore #16?", answer: "1", options: ["Demarcus Cousins","Peja Stojakovic","Hedo Türkoğlu", "Omri Casspi"]},
+    {gif: "assets/images/Oscar.gif",question: "Who leads the franchise in assists?", answer: "3", options: ["Kevin Martin","Darren Collison","Doug Christie", "Oscar Robinson"]},
+    {gif: "assets/images/Marvin.gif",question: "What year did the Golden 1 Center open?", answer: "2", options: ["2012","2017","2016", "2009"]},
+    {gif: "assets/images/Deaaron.gif",question: "What pick number was De'aaron Fox in the 2017 NBA Draft?", answer: "0", options: ["5","1","9", "7"]},
+    {gif: "assets/images/Bogi.gif",question: "What position does Bogdan Bogdanović play?", answer: "2", options: ["Point Guard","Power Forward","Shooting Guard", "Center"]},
+    {gif: "assets/images/VladeDance.gif",question: "Which past Kings player doesn't currently work with the Kings?", answer: "1", options: ["Bobby Jackson","Chris Webber","Vlade Divac", "Doug Christie"]},
 ];
 
-$(".title").text("Trivia Game");
+$(".title").html("<h1>Sacramento Kings Trivia</h1>");
 var $button = $("<button>");
 $button.addClass("start");
 $button.text("Start");
@@ -29,22 +35,6 @@ $(".start").on("click", function(){
     intervalID = setInterval(countDown, 1000);
 });
 
-function resetTrivia(){
-    timeLimit = 3;
-    time = timeLimit;
-    delayTime = 0;
-    ansSelected = false;
-    ans = -1;
-    curQ = 0;
-    correct = 0;
-    wrong = 0;
-    timeOut = 0;
-    $(".title").empty();
-    $(".starter").empty();
-    $("#timer").empty();
-    $("#question").empty();
-    $("#answers").empty();
-}
 function countDown(){
     if(time >= 0 && ansSelected === false){
         setDisplay();
@@ -69,7 +59,7 @@ function countDown(){
 }
 
 function delayResult(){
-    if(delayTime <= 2){
+    if(delayTime <= 1){
         delayTime++;
         console.log("Delay: " + delayTime);
         ansSelected = false;
@@ -120,9 +110,10 @@ function displayEnd(){
     var $correct = $("<div>");
     var $wrong = $("<div>");
     var $timeOut = $("<div>");
-    $correct.text("Correct: "+correct);
-    $wrong.text("Wrong: "+wrong);
-    $timeOut.text("Timeout: "+timeOut);
+    $("#timer").html("<h2>This is how you did!</h2>");
+    $correct.html("<h3>Correct: "+correct+"</h3>");
+    $wrong.html("<h3>Wrong: "+wrong+"</h3>");
+    $timeOut.html("<h3>Timeout: "+timeOut+"</h3>");
     $("#answers").append($correct);
     $("#answers").append($wrong);
     $("#answers").append($timeOut);
@@ -141,9 +132,12 @@ function displayResult(result){
     $("#timer").empty();
     $("#question").empty();
     $("#answers").empty();
-    $("#timer").text("Time Remaining: "+ (time + 1));
-    $("#question").text(result);
-    $("#answers").text("Correct Answer: " + questions[curQ].options[questions[curQ].answer]);
+    var $gif = $("<div>");
+    $gif.html("<img src=" + questions[curQ].gif +">")
+    $("#timer").html("<h4>Time Remaining: "+ (time)+"</h4>");
+    $("#question").html("<h3>"+result+"</h3>");
+    $("#question").append($gif);
+    $("#answers").html("<h4>Correct Answer: " + questions[curQ].options[questions[curQ].answer]+"</h4>");
 }
 
 // Sets up the display during the quiz
@@ -151,8 +145,8 @@ function setDisplay(){
     $("#timer").empty();
     $("#question").empty();
     $("#answers").empty();
-    $("#timer").text("Time Remaining: "+time);
-    $("#question").text(questions[curQ].question);
+    $("#timer").html("<h4>Time Remaining: "+(time-1)+"</h4>");
+    $("#question").html("<h3>"+questions[curQ].question+"</h3>");
     for(var i = 0; i < questions[curQ].options.length; i++){
         var $button = $("<button>");
         $button.addClass("options");
@@ -161,4 +155,21 @@ function setDisplay(){
         $("#answers").append($button);
     }
     finishButton();
+}
+
+// Sets up the game to be replayed
+function resetTrivia(){
+    timeLimit = 15;
+    time = timeLimit;
+    delayTime = 0;
+    ansSelected = false;
+    ans = -1;
+    curQ = 0;
+    correct = 0;
+    wrong = 0;
+    timeOut = 0;
+    $(".starter").empty();
+    $("#timer").empty();
+    $("#question").empty();
+    $("#answers").empty();
 }
